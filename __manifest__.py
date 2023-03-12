4d9034ca1872c38a090a3afec11e167fd29e1175
Enrique Carretero Tato <a19enriquect@iessanclemente.net>
2023-03-12 12:42:26 +0100

    Primer commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "todolistEnriqueCT",
+
+    'summary': """
+        Módulo que complementa todolist con otras funcionalidades.""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Enrique Carretero Tato",
+    'website': "https://www.iessanclemente.net",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Tools',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        
+    ],
+}
